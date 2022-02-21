63903d7d88ef35be4852a90e39b36dc1649cc490
DmytroStelmakh <dstelmakh02@gmail.com>
2022-02-21 20:08:39 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+const arrayItems = [];
+
+ingredients.forEach(el => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('.item');
+  listItem.textContent = el;
+  arrayItems.push(listItem);
+});
+
+ingredientsList.append(...arrayItems);
+console.log(ingredientsList);
