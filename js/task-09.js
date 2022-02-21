7aaff3751720455a39540a794ce84e48b03af9de
DmytroStelmakh <dstelmakh02@gmail.com>
2022-02-21 21:04:59 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyElement = document.querySelector('body');
+const currentColor = document.querySelector('.color');
+const btnChangeColor = document.querySelector('.change-color');
+
+btnChangeColor.addEventListener('click', changeBackgroundColor);
+
+function changeBackgroundColor() {
+  const randomColor = getRandomHexColor();
+  bodyElement.style.backgroundColor = randomColor;
+  currentColor.textContent = randomColor;
+}
