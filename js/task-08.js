7b727e9d88a9daf9687463a26e44c29006d9759f
DmytroStelmakh <dstelmakh02@gmail.com>
2022-02-21 20:52:26 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+  if (email && password) {
+    console.dir({ email, password });
+  } else {
+    alert('все поля должны быть заполнены');
+  }
+
+  form.reset();
+}
